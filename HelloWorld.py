df84f81cd9b0f3a76a9b16edc7f622682afe834e
Erosssore <111210204+Erosssore@users.noreply.github.com>
2025-03-26 12:10:16 -0500

    Added comments.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,13 @@
+# Name class takes inputs from n which is then converted to a Name object and passed into the world() function
 class Name:
+    # OOP Initialization and Function
     def __init__(self, name):
         self.name = name
     def world(self):
         print("\nHello World, my name is", self.name, "\n"
               "this is the SCM project!")
 
+# Input segment and function call
 n = input("What is your name: ")
 user = Name(n)
 user.world()
